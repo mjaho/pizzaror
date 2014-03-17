61ad645e3572b1f8bc15505011aba22cc2bfa6bf
Mika Aho <mjaho@cs.helsinki.fi>
2014-03-17 13:08:16 +0200

    aloitus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pizzaror::Application.config.secret_key_base = '8ed1b8a33a304ab44da979f1c83813413fccf8c841ec28aabc85065e1c0d75032604ffe92c12ac65825b26b2b75dc267c1b35908ee32ab65c9318d8b7bb1067e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pizzaror::Application.config.session_store :cookie_store, key: '_Pizzaror_session'
